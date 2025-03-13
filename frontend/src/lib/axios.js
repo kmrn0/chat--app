4e8c7758182f75d27b90f81edbc375dc47da471f
Kamran Ansari <karanansari9919@gmail.com>
2025-03-13 20:58:12 +0530

    change env for frontend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,14 @@
-import axios from "axios"
+// import axios from "axios"
+
+// export const axiosInstance = axios.create({
+//     baseURL: "http://localhost:5001/api",
+//     withCredentials: true,
+// })     
+
+
+import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5001/api",
+    baseURL: import.meta.env.VITE_API_URL || "http://localhost:5001/api",
     withCredentials: true,
-})     
+});
